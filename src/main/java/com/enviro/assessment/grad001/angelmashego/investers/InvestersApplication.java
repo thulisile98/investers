cca19707082a65d3f98499e15c9d7c8b18dfc42d
Angel Mashego <thulisile.ma98@gmail.com>
2023-09-18 13:53:05 +0200

    Added dependencies and apllication properties

---
+++ src/main/java/com/enviro/assessment/grad001/angelmashego/investers/InvestersApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.angelmashego.investers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestersApplication.class, args);
+	}
+
+}
