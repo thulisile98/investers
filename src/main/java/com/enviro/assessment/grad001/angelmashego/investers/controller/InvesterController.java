06eaedb504ee6b711e7a713cbf2ba830cb83d118
Angel Mashego <thulisile.ma98@gmail.com>
2023-09-19 20:42:07 +0200

    annotated my controller class with RestController to combine my  controller and repository and injected Invester repository  and for posting data into my H2 db added the  PostMapping annotation

--- src/main/java/com/enviro/assessment/grad001/angelmashego/investers/controller/InvesterController.java
+++ src/main/java/com/enviro/assessment/grad001/angelmashego/investers/controller/InvesterController.java
@@ -1,4 +1,21 @@
 package com.enviro.assessment.grad001.angelmashego.investers.controller;
 
+import com.enviro.assessment.grad001.angelmashego.investers.model.Invester;
+import com.enviro.assessment.grad001.angelmashego.investers.repository.InvesterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InvesterController {
+
+    @Autowired
+    private InvesterRepository investerRepository;
+
+    @PostMapping("/INVESTER")
+    Invester newInvester(@RequestBody Invester newInvester){
+        return  investerRepository.save(newInvester);
+    }
+
 }
